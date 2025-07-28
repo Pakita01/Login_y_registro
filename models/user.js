872ec0c2b22ea6b100164482559dd5a40fca6932
Pakita Dubreus <pakitastudio@gmail.com>
2025-07-28 11:37:16 -0400

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,28 @@
+const { default: mongoose } = require('mongoose');
+const monggoose= require('mongoose');
+
+const userSchema = new monggoose.Schema({
+    name:  String,  
+    email:  String,
+    passwordHash: String,
+    todos:{
+        type:Boolean,
+        default:false
+    },  
+    leccion: [{
+         type: mongoose.Schema.Types.ObjectId,
+         ref: 'User'
+    }]
+});
+
+ userSchema.set('toJSON', {
+    transform:(document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+        delete returnedObject.passwordHash; // No enviar el hash de la contraseña
+    }
+});
+
+const User = mongoose.model('User',userSchema);
+module.exports = User;
